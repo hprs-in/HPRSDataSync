04f4f18ce40cfa73615886386a853aad8ec2b870
parthsanepara <parthsanepara@gmail.com>
2024-08-26 08:28:25 +0530

    add: sonar test

---
+++ main.c
@@ -0,0 +1,18 @@
+/**
+ * @file main.c
+ * @author Parth Sanepara
+ * @brief 
+ * @version 0.1
+ * @date 2024-08-26
+ * 
+ * @copyright Copyright (c) 2024
+ * 
+ */
+
+#include <stdio.h>
+
+int main(int argc, char const *argv[])
+{
+    printf("Hello World!\n");
+    return 0;
+}
